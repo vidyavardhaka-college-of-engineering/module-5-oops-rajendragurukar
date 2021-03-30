564336769d72711ef79682edaef2c8c78a93a300
rajendragurukar <20ec0027@vvce.ac.in>
2021-03-30 10:18:27 +0000

    done

--- Program-4.cpp
+++ Program-4.cpp
@@ -11,7 +11,7 @@ int main()
     int p,t,r,s; //declare the varibales
     std::cout << "Enter the values of p,t,r: "; //find the missing statement
     std::cin >> p >> t >> r;//find the missing statement
-                            //find the missing expression to calculate simple interest
+    s=(p*t*r)/100;                        //find the missing expression to calculate simple interest
     std::cout << s;  
     return 0;
 }
